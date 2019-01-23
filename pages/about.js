5ebf498711b830de18a1f6d11d9133c2977a94f7
Juno Julius <yunoyulius@gmail.com>
2019-01-23 21:38:36 +0100

    init commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+
+const about = () => (
+    <div>
+    <h1>About Bitzprix</h1>
+    <p>An application to view Bitcoin prices</p>
+    </div>
+    );
+    
+    export default about;
+    
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+
+const index = () => (
+<div>
+
+<h1>Welcome to Bitzprix</h1>
+</div>
+);
+
+export default index;
